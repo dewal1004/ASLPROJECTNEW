02677aead8290fdec7a70472139f6ba0b602d541
dewal1004 <dewaliii@hotmail.com>
2023-02-20 08:11:33 +0000

    CAL

--- PS/CALtoAL -Payroll.ps1
+++ PS/CALtoAL -Payroll.ps1
@@ -1,9 +1,19 @@
 https://learn.microsoft.com/en-us/dynamics365/business-central/dev-itpro/upgrade/devenv-code-conversion?source=docs
 
 # Code Conversion from C/AL to AL
+# Import the NAV development environment module
 # Task 1: Import the test library into your C/AL solution
 # Task 2: Compile all the objects in your C/AL solution
-$DbName=  "Demo Database BC (14-0)"
+{
+    # Example
+    # Compile-NAVApplicationObject MyApp
+    # Compile-NAVApplicationObject MyApp -Filter 'Type=Codeunit' -Recompile
+    # 'Page','Codeunit','Table','XMLport','Report','Query' | % { Compile-NAVApplicationObject -DatabaseName MyApp -Filter "Type=$_" -AsJob } | Receive-Job -Wait
+}
+    
+Compile-NAVApplicationObject "Demo Database BC (14-0)" -Filter 'Type=Codeunit' -Recompile
+    
+$DbName = "Demo Database BC (14-0)"
 Compile-NAVApplicationObject $DbName 
 Compile-NAVApplicationObject $DbName -Filter 'Version List='*@SSNL*'' -Recompile
 # Task 3: Export the application objects to the new TXT syntax
@@ -14,7 +24,7 @@ mkdir c:\export2al\baseapplication
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part2.txt" -Filter 'Id=140000..1999999999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14testobjects.txt" -Filter 'Id=130400..130416'
 $DbSvr = 'NAV365TEST\ASLHO2016' 
-$DbName=  "Demo Database BC (14-0)"
+$DbName = "Demo Database BC (14-0)"
 
 $Path = "c:\export2al\baseapplication\exportedbc14app-part1.txt"
 $Filter = 'Id=1..129999'
@@ -33,7 +43,7 @@ Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $DbName -Export
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part1.txt" -Filter 'Id=1..129999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part2.txt" -Filter 'Id=140000..1999999999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14testobjects.txt" -Filter 'Id=130400..130416'
-$DbName =  "Demo Database BC (14-0)"
+$DbName = "Demo Database BC (14-0)"
 $DbSvr = ".\BCDEMO"
 
 ## 5.3.1 Make target path if not existing:  C:\export2al\bc14tablesonly
@@ -46,11 +56,13 @@ $ExportToPath = "C:\export2al\bc14tablesonly\exportedbc14-tables1.txt"
 $ExportFilter = 'Type=Table;Id=1..1999999999'
 
 Export-NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables1.txt" -Filter 'Type=Table;Id=1..1999999999'
-Export-NAVApplicationObject -DatabaseServer .\BCDEMO             -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables.txt" -Filter 'Type=Table;Id=1..1999999999'
-Export-NAVApplicationObject -DatabaseServer $DbSrvr -DatabaseName $DbName -ExportToNewSyntax -Path $ExportToPath -Filter 'Type=Table;Id=1..1999999999'
-Export-NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Atlantic ASL V14toV18" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\2\exportedbc14-tables2.txt" -Filter 'Type=Table;Id=1..1999999999'
+Export-NAVApplicationObject -DatabaseServer .\BCDEMO             -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables.txt" -Filter 'Type=Table;Id=1..1999999Export-NAVApplicationObject -DatabaseServer $DbSrvr -DatabaseName $DbName -ExportToNewSyntax -Path $ExportToPath -Filter 'Type=Table; Id=1..1999999999'
+Exportcls
+-NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Atlantic ASL V14toV18" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\2\exportedbc14-tables2.txt" -Filter 'Type=Table; Id=1..1999999999'
  
-##5.4: Use the txt2al conversion 
+class ClassName {
+    <# Define the class. Try constructors, properties, or methods. #>
+}##5.4: Use the txt2al conversion 
 mkdir C:\export2al\bc14tablesonly\al
 # run CMD as administrator
 # change to txt2al dir, By default, the location is 
