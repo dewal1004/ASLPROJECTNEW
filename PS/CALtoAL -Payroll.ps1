34522076af1a0ed83d2dbddeaaa30151e43880b5
dewal1004 <dewaliii@hotmail.com>
2023-03-01 11:14:04 +0000

    Payroll Payslip

--- PS/CALtoAL -Payroll.ps1
+++ PS/CALtoAL -Payroll.ps1
@@ -2,6 +2,9 @@ https://learn.microsoft.com/en-us/dynamics365/business-central/dev-itpro/upgrade
 
 # Code Conversion from C/AL to AL
 # Import the NAV development environment module
+# import-Module "C:\Program Files\Microsoft Dynamics 365 Business Central\140\Service\navAdminTool.ps1" //BC140 path
+Import-Module "C:\Program Files\Microsoft Dynamics NAV\90\Service\navAdminTool.ps1"
+Import-Module "${env:ProgramFiles(x86)}\Microsoft Dynamics NAV\90\RoleTailored Client\Microsoft.Dynamics.Nav.Model.Tools.psd1" -force
 # Task 1: Import the test library into your C/AL solution
 # Task 2: Compile all the objects in your C/AL solution
 {
@@ -15,9 +18,9 @@ Compile-NAVApplicationObject "Demo Database BC (14-0)" -Filter 'ID=50000..50100'
 Compile-NAVApplicationObject "Demo Database BC (14-0)" -logpath "C:\Temp" -Filter 'Version List=*SSN1.0*' -Recompile -SynchronizeSchemaChanges force
 get-help Compile-NAVApplicationObject -Online
     
-$DbName = "Demo Database BC (14-0)"
-Compile-NAVApplicationObject $DbName 
-Compile-NAVApplicationObject $DbName -Filter 'Version List='*@SSNL*'' -Recompile
+$dbName = "Demo Database BC (14-0)"
+Compile-NAVApplicationObject $dbName 
+Compile-NAVApplicationObject $dbName -Filter 'Version List='*@SSNL*'' -Recompile
 # Task 3: Export the application objects to the new TXT syntax
 # 1.a Create a folder for the exported base application objects to TXT files (for example, 
 mkdir c:\export2al\baseapplication
@@ -26,26 +29,26 @@ mkdir c:\export2al\baseapplication
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part2.txt" -Filter 'Id=140000..1999999999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14testobjects.txt" -Filter 'Id=130400..130416'
 $DbSvr = 'NAV365TEST\ASLHO2016' 
-$DbName = "Demo Database BC (14-0)"
+$dbName = "Demo Database BC (14-0)"
 
 $Path = "c:\export2al\baseapplication\exportedbc14app-part1.txt"
 $Filter = 'Id=1..129999'
-Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $DbName -ExportToNewSyntax -Path $Path -Filter $Filter
+Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $dbName -ExportToNewSyntax -Path $Path -Filter $Filter
 
 $Path = "c:\export2al\baseapplication\exportedbc14app-part2.txt"
 $Filter = 'Id=140000..1999999999'
-Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $DbName -ExportToNewSyntax -Path $Path -Filter $filter
+Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $dbName -ExportToNewSyntax -Path $Path -Filter $filter
 
 $Path = "c:\export2al\baseapplication\exportedbc14testobjects.txt"
 $Filter = 'Id=130400..130416'
-Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $DbName -ExportToNewSyntax -Path $Path -Filter $Filter
+Export-NAVApplicationObject -DatabaseServer $DbSvr -DatabaseName $dbName -ExportToNewSyntax -Path $Path -Filter $Filter
 
 # 2.a Create a folder
 # 1.b Export the application objects
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part1.txt" -Filter 'Id=1..129999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14app-part2.txt" -Filter 'Id=140000..1999999999'
 #   Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "c:\export2al\baseapplication\exportedbc14testobjects.txt" -Filter 'Id=130400..130416'
-$DbName = "Demo Database BC (14-0)"
+$dbName = "Demo Database BC (14-0)"
 $DbSvr = ".\BCDEMO"
 
 ## 5.3.1 Make target path if not existing:  C:\export2al\bc14tablesonly
@@ -53,14 +56,18 @@ mkdir C:\export2al\bc14tablesonly
 # 5.3.2 Export tables e.g. 
 # Export-NAVApplicationObject -DatabaseServer .\BCDEMO -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables.txt" -Filter 'Type=Table;Id=1..1999999999'
 $DbSrvr = 'NAV365TEST\ASLHO2016' 
-$DbName = "Demo Database BC (14-0)" 
+$dbName = "Demo Database BC (14-0)" 
 $ExportToPath = "C:\export2al\bc14tablesonly\exportedbc14-tables1.txt" 
 $ExportFilter = 'Type=Table;Id=1..1999999999'
 
 Export-NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables1.txt" -Filter 'Type=Table;Id=1..1999999999'
-Export-NAVApplicationObject -DatabaseServer .\BCDEMO             -DatabaseName "Demo Database BC (14-0)" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables.txt" -Filter 'Type=Table;Id=1..1999999Export-NAVApplicationObject -DatabaseServer $DbSrvr -DatabaseName $DbName -ExportToNewSyntax -Path $ExportToPath -Filter 'Type=Table; Id=1..1999999999'
-Exportcls
--NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Atlantic ASL V14toV18" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\2\exportedbc14-tables2.txt" -Filter 'Type=Table; Id=1..1999999999'
+Export-NAVApplicationObject -DatabaseServer .\BCDEMO             -DatabaseName "Demo Database BC (14-0)" 
+-ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\exportedbc14-tables.txt" 
+-Filter 'Type=Table;Id=1..1999999'
+
+Export-NAVApplicationObject -DatabaseServer $DbSrvr -DatabaseName $dbName -ExportToNewSyntax -Path $ExportToPath -Filter 'Type=Table; Id=1..1999999999'
+
+Exportcls-NAVApplicationObject -DatabaseServer NAV365TEST\ASLHO2016 -DatabaseName "Atlantic ASL V14toV18" -ExportToNewSyntax -Path "C:\export2al\bc14tablesonly\2\exportedbc14-tables2.txt" -Filter 'Type=Table; Id=1..1999999999'
  
 class ClassName {
     <# Define the class. Try constructors, properties, or methods. #>
@@ -88,3 +95,17 @@ There is a profile/role that allows you to load module automatically on PS, Shar
 
 Get-Command -Name '*Compile*'
 
+# Get Database
+get-command -Noun SqlDatabase* -Verb Get
+Get-Help Get-SqlDatabase -Examples
+Get-SqlInstance -Credential $credential -MachineName " MachineName" | Get-SqlDatabase -Credential $sqlCredential;
+Get-SqlInstance
+
+ServerName : ABIOLA-HP\BCNAV
+DatabaseName : "Demo Database NAV (9-0)"
+Compile-NAVApplicationObject "Demo Database NAV (9-0)" -Filter 'ID=50000..50100' 
+Get-Command -name *windowsUser*
+Get-Command -noun *windows* -verb get
+Get-Command -noun *sql* -verb get
+
+User 'MicrosoftAccount\dewaliii@hotmail.com'
