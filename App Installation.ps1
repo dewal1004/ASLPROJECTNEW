43ae208352da8e718d10ff8d209235ca6542af29
Abiola Adewale <dewaliii@hotmail.com>
2023-04-16 11:28:11 +0100

    Lauch  App

--- App Installation.ps1
+++ App Installation.ps1
@@ -1,14 +1,14 @@
 ﻿import-Module "C:\Program Files\Microsoft Dynamics 365 Business Central\180\Service\navAdminTool.ps1"
 
-UnInstall-NavApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.86
-Unpublish-NavApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.86
+UnInstall-NavApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.90
+Unpublish-NavApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.90
 
-Publish-NAVApp -ServerInstance BC180 -Path "C:\Als\ASLPROJECTNEW\SSNL_ASL Upgrade Operation_14.5.1.87.app" -SkipVerification
-Import-NavserverLicense -LicenseFile C:\LIC\SSNL36504.flf -ServerInstance BC180
+Publish-NAVApp -ServerInstance BC180 -Path "C:\Als\ASLPROJECTNEW\SSNL_ASL Upgrade Operation_14.5.1.92.app" -SkipVerification
+Import-NavserverLicense -LicenseFile C:\LIC\SSNL36505.flf -ServerInstance BC180
 Restart-NAVServerInstance BC180
 
-Install-NAVApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.87
+Install-NAVApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.92
 Start-NAVAppDataUpgrade 
-Sync-NAVApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.87
+Sync-NAVApp -ServerInstance BC180 -Name "ASL Upgrade Operation" -Version 14.5.1.88
 Import-NavserverLicense -LicenseFile C:\LIC\Shod36504.flf -ServerInstance BC180
 Restart-NAVServerInstance BC180
